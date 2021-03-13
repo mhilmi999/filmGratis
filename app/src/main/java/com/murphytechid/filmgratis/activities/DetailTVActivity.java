f1627bf7ed4cd0789c734b39a2b910ac8b8dce88
Muhammad Hilmi Ramadhan <hilmi.muhammad.rmdhn@gmail.com>
2021-03-13 11:38:39 +0700

    Fiksasi final semua fitur baik sisi film, tv, notifikasi pengingat, dan favorit sementara

--- app/src/main/java/com/murphytechid/filmgratis/activities/DetailTVActivity.java
+++
@@ -1,6 +0,0 @@
-package com.murphytechid.filmgratis.activities;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class DetailTVActivity extends AppCompatActivity {
-}
--- app/src/main/java/com/murphytechid/filmgratis/activities/MainActivity.java
+++ app/src/main/java/com/murphytechid/filmgratis/activities/MainActivity.java
@@ -1,57 +1,98 @@
 package com.murphytechid.filmgratis.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.murphytechid.filmgratis.R;
+import com.murphytechid.filmgratis.fragment.FragmentFavorite;
+import com.murphytechid.filmgratis.fragment.FragmentMovie;
+import com.murphytechid.filmgratis.fragment.FragmentTV;
+import com.murphytechid.filmgratis.utils.BottomBarBehavior;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.Fragment;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment = null;
+    ImageView imgNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        imgNotification = findViewById(R.id.imgNotification);
+        imgNotification.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BubbleNavigationLinearView navigationBar = findViewById(R.id.navigationBar);
+
+        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigationBar.getLayoutParams();
+        layoutParams.setBehavior(new BottomBarBehavior());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FragmentMovie()).commit();
+
+        navigationBar.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                switch (position) {
+                    case 0:
+                        fragment = new FragmentMovie();
+                        break;
+                    case 1:
+                        fragment = new FragmentTV();
+                        break;
+                    case 2:
+                        fragment = new FragmentFavorite();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
         }
-
-        return super.onOptionsItemSelected(item);
+        window.setAttributes(winParams);
     }
+
+
 }
